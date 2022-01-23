19a3d5879ff423297a3180b98986c60c7cf12b32
Stathis Kapnidis <ek364@cs33018804.pws.uscs.susx.ac.uk>
2022-01-23 17:35:47 +0000

    Looks through the Searches folder and tries to filter out which is the newest file. It doesn't succeed

--- include/URL.h
+++ include/URL.h
@@ -3,10 +3,16 @@
 #include <string.h>
 #include <errno.h>
 #include <curl/curl.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <dirent.h>
 #include <time.h>
+#include <sys/stat.h>
+#include <stdint.h>
 
 #define CUSTOM_SEARCH_ENGINE_URL "https://customsearch.googleapis.com/customsearch/v1?"
 
 size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream);
 char * Build_Url(char * querry);
 void error(char * msg, int code);
+char *isDirectoryEmpty(char *dirname);
--- src/URL.c
+++ src/URL.c
@@ -13,14 +13,30 @@ size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 char * Build_Url(char * querry)
 {
   char *url = malloc(1024);
+  char start[50];
+  char *file_name;
+  strcpy(start,"&start=");
+  if((file_name = isDirectoryEmpty("Searches")) == NULL)
+    printf("Directory is NOT empty\nLatest File: %s\n",file_name);
+  else{
+    strcat(start,"1");
+    printf("Directory is Empty\n");
+  }
 
-  sprintf(url,"%s%s%s%s%s",                         \
+    
+    
+  
+
+
+  sprintf(url,"%s%s%s%s%s%s",                         \
       CUSTOM_SEARCH_ENGINE_URL,                     \
       Custom_Search_Engine_cx,                      \
       "&q=",                                        \
       curl_easy_escape(curl,querry,strlen(querry)), \
+      "&start=101",
       Custom_Search_Engine_Key                      \
       );
+  
 
   return url;
 }
@@ -29,4 +45,60 @@ void error(char * msg, int code)
 {
   fprintf(stderr, "%s: %s\n",msg, strerror(errno));
   exit(code);
+}
+
+char * isDirectoryEmpty(char *dirname)
+{
+  int n = 0;
+  struct dirent *d;
+  struct stat stats;
+  struct tm dt;
+  time_t time, time_new;
+  char *file_name = malloc(255);
+  DIR *dir = opendir(dirname);
+  char file_path[255];
+
+  if (dir == NULL) //Not a directory or doesn't exist
+    return NULL;
+  printf("Reading \t%s\n",dirname);
+
+  if((d = readdir(dir)) != NULL){
+    n++;
+    printf("%d - %s\n", n, d->d_name);
+    sprintf(file_path,"%s/%s",dirname,d->d_name);
+    if(stat(file_path, &stats) == 0){
+      time = stats.st_ctime;
+      sprintf(file_name,"%s/%s",dirname, d->d_name);
+    }
+  }
+  else
+    return NULL;
+  
+
+  while ((d = readdir(dir)) != NULL){
+    n++;
+    printf("%d - %s\n", n, d->d_name);
+    sprintf(file_path,"%s/%s",dirname,d->d_name);
+    if(stat(d->d_name, &stats) == 0){
+      time_new =  stats.st_ctime;
+    }
+    printf("time :%ld\ttime_new :%ld\n",time,time_new);
+    if(time_new < time){
+      sprintf(file_name,"%s/%s",dirname, d->d_name);
+      time = time_new;
+    }
+    else
+    {
+      printf("OOPs\n");
+    }
+    
+  }
+  printf("Latest File: \t%s\n",file_name);
+  printf("Closing\t%s\n",dirname);
+  closedir(dir);
+
+  if (n <= 2) //Directory Empty
+    return NULL;
+  else
+    return file_name;
 }
